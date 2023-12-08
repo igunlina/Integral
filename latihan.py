995f9e524a85d582b8b3dffea0123e45866d29cf
igunlina <152131762+igunlina@users.noreply.github.com>
2023-12-08 14:15:56 +0700

    Update latihan.py

--- latihan.py
+++ latihan.py
@@ -8,40 +8,17 @@ def main():
         st.title('BISMILLAH')
         st.header('MARI BELAJAR INTEGRAL TAK TENTU (INDEFINITE INTEGRAL/PRIMITIVE\ANTIDERIVATIVE)')
         
+        # Pertanyaan 1
         answer_1 = st.radio("Tentukan primitive dari fungsi $f(x)=2x$", ["$F(x)=x^2$", "$F(x)=2x^2$", "$F(x)=4$"]) 
-         # Pertanyaan 2
-        answer_2 = st.radio("Tentukan primitive dari fungsi $f(x)=x^3$", ["$F(x)= \\frac{1}{3} x^3$", "$F(x)=2x$", "$F(x)=\\frac{1}{2} x^2$"])
+        # Pertanyaan 2
+        answer_2 = st.radio("Tentukan primitive dari fungsi $f(x)=x^2$", ["$F(x)= \\frac{1}{3} x^3$", "$F(x)=2x$", "$F(x)=\\frac{1}{2} x^2$"])
 
         # Pertanyaan 3
         answer_3 = st.radio("Tentukan primitive dari fungsi $f(x)=0$", ["$F(x)=0$", "$F(x)=C \in \mathbb{R}$", "$F(x)=3x^2$"])
         # Tombol Kuis
         if st.button("submit"):
             check_definisi_answers(answer_1, answer_2, answer_3)
-            #check_latihan_answers(answer_1)
-            #run_latihan_quiz()
-
-def run_latihan_quiz():
-    # Pertanyaan 1
-    answer_1 = st.radio("Tentukan primitive dari fungsi $f(x)=2x$", ["$F(x)=x^2$", "$F(x)=2x^2$", "$F(x)=4$"])
- 
-    # Tombol Submit
-    submitted = st.button("Submit")
-    if st.button("submit"):
-        check_latihan_answers(answer_1)
-        st.title('lewat sini')
-    if submitted:
-        st.title('lewat sini')
-        check_latihan_answers(answer_1)
-
-def check_latihan_answers(answer_1):
-    # Logika pengecekan jawaban dan memberikan umpan balik
-    correct_answers = {"$F(x)=x^2$": "Benar"}
-
-    if answer_1 in correct_answers :
-        st.success("Selamat! Jawaban Anda benar.")
-    else:
-        st.error("Mohon maaf, jawaban Anda salah. Silahkan diulangi kembali.")
-
+            
 def check_definisi_answers(answer_1, answer_2, answer_3):
     # Logika pengecekan jawaban dan memberikan umpan balik
     correct_answers = {"$F(x)=x^2$": "Benar", "$F(x)= \\frac{1}{3} x^3$": "Benar", "$F(x)=0$": "Benar"}
